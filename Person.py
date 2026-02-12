884203c7e545c4e56d05708a2725b1dc2d9ca8be
Mohamed hakim Eldargaly <mohamedhakimmon3@gmail.com>
2026-02-12 16:58:43 +0200

    Add files via upload

---
+++ Person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def view_info(self):
+        return "Name: " + self.name + " | Age: " + str(self.age)
+    
+    X=Person("mohamed", 20)
+    print(X.view_info())
+    
